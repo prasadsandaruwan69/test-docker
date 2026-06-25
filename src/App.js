fb9765f2e149c1fb514e45973f94a981217c583f
Prasad Sadaruwan <prasadsandaruwan69@gmail.com>
2026-06-25 13:55:45 +0530

    Fix label text for username input in login form

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <h1>Login1</h1>
         <form onSubmit={handleSubmit} className="login-form">
           <label>
-            Username
+            name
             <input
               type="text"
               value={username}
