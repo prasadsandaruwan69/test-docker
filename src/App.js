f0184e248a40c5e55560475d5cb72fc8b5264c35
Prasad Sadaruwan <prasadsandaruwan69@gmail.com>
2026-06-25 15:09:46 +0530

    Update header text in login card for consistency

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <div className="login-card">
-        <h1>Login1</h1>
+        <h1> set</h1>
         <form onSubmit={handleSubmit} className="login-form">
           <label>
             nameuser
