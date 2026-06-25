e5c171443640894ab039f7f62996570d9faaec22
Prasad Sadaruwan <prasadsandaruwan69@gmail.com>
2026-06-25 17:10:40 +0530

    Update header text in login card for clarity

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <div className="login-card">
-        <h1> setkuashia</h1>
+        <h1> gon thmal path</h1>
         <form onSubmit={handleSubmit} className="login-form">
           <label>
             nameuser
